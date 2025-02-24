88bc1e112fb520a9b9f69df0edc9472fb1bdca24
Jens von Bergmann <jens@mountainmath.ca>
2025-02-24 03:20:02 +0000

    version 0.4

--- inst/doc/working_with_large_tables.R
+++ inst/doc/working_with_large_tables.R
@@ -6,22 +6,31 @@ knitr::opts_chunk$set(
 )
 
 ## ----setup--------------------------------------------------------------------
-library(dplyr)
+library(dplyr, warn.conflicts = FALSE)
 library(ggplot2)
 
 library(cansim)
 
 ## -----------------------------------------------------------------------------
-connection <- get_cansim_sqlite("20-10-0001")
+connection.parquet <- get_cansim_connection("20-10-0001") # format='parquet' is the default
+
+glimpse(connection.parquet)
 
 ## -----------------------------------------------------------------------------
-head(connection)
+connection.feather <- get_cansim_connection("20-10-0001", format='feather')
+
+glimpse(connection.feather)
+
+## -----------------------------------------------------------------------------
+connection.sqlite <- get_cansim_connection("20-10-0001", format='sqlite')
+
+glimpse(connection.sqlite)
 
 ## -----------------------------------------------------------------------------
 get_cansim_table_overview("20-10-0001")
 
 ## -----------------------------------------------------------------------------
-data <- connection %>%
+data.parquet <- connection.parquet %>%
   filter(GEO=="Canada",
          `Seasonal adjustment`=="Unadjusted",
          Sales=="Units",
@@ -29,10 +38,42 @@ data <- connection %>%
          `Vehicle type` %in% c("Passenger cars","Trucks")) %>%
   collect_and_normalize()
 
-data %>% head()
+data.parquet %>% head()
 
 ## -----------------------------------------------------------------------------
-data %>%
+data.feather <- connection.feather %>%
+  filter(GEO=="Canada",
+         `Seasonal adjustment`=="Unadjusted",
+         Sales=="Units",
+         `Origin of manufacture`=="Total, country of manufacture",
+         `Vehicle type` %in% c("Passenger cars","Trucks")) %>%
+  collect_and_normalize()
+
+data.feather %>% head()
+
+## -----------------------------------------------------------------------------
+data.sqlite <- connection.sqlite %>%
+  filter(GEO=="Canada",
+         `Seasonal adjustment`=="Unadjusted",
+         Sales=="Units",
+         `Origin of manufacture`=="Total, country of manufacture",
+         `Vehicle type` %in% c("Passenger cars","Trucks")) %>%
+  collect_and_normalize()
+
+data.sqlite %>% head()
+
+## -----------------------------------------------------------------------------
+data.memory <- get_cansim("20-10-0001") %>%
+  filter(GEO=="Canada",
+         `Seasonal adjustment`=="Unadjusted",
+         Sales=="Units",
+         `Origin of manufacture`=="Total, country of manufacture",
+         `Vehicle type` %in% c("Passenger cars","Trucks")) 
+
+data.memory %>% head()
+
+## -----------------------------------------------------------------------------
+data.parquet %>%
   filter(Date>=as.Date("1990-01-01")) %>%
   ggplot(aes(x=Date,y=val_norm,color=`Vehicle type`)) +
   geom_smooth(span=0.2,method = 'loess', formula = y ~ x) +
@@ -42,11 +83,9 @@ data %>%
        x=NULL,y="Number of units")
 
 ## -----------------------------------------------------------------------------
-disconnect_cansim_sqlite(connection)
+list_cansim_cached_tables()
 
 ## -----------------------------------------------------------------------------
-list_cansim_sqlite_cached_tables()
-
-## -----------------------------------------------------------------------------
-remove_cansim_sqlite_cached_table("20-10-0001")
+disconnect_cansim_sqlite(connection.sqlite)
+remove_cansim_cached_tables("20-10-0001")
 
